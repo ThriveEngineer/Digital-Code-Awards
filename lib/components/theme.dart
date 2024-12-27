ec6e23faecfa562083bece2676238eb1ef4d5f38
Luis Schröder <kresstudios@gmail.com>
2024-12-27 13:44:37 +0100

    final version

--- lib/components/theme.dart
+++ lib/components/theme.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
-  fontFamily: "Nothing",
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
     surface: Colors.black,
